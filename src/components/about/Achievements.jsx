9c385741f9484d2475186af816eeac18110ff6e3
Alejandro Lamas <alejandro.lamas@colvet.es>
2024-12-03 00:11:06 +0100

    Achievements

--- src/components/about/Achievements.jsx
+++ src/components/about/Achievements.jsx
@@ -2,21 +2,13 @@ import React from "react";
 
 const achievementsContent = [
   { title: "10", subTitle1: "Años de", subTitle2: "experiencia" },
-  { title: "97", subTitle1: "Desarrollo", subTitle2: "Wordpress" },
-  { title: "25", subTitle1: "Desarrollo", subTitle2: "Prestashop" },
-  { title: "11", subTitle1: "Desarrollo", subTitle2: "Shopify" },
-  { title: "2", subTitle1: "Desarrollo", subTitle2: "Drupal" },
-  { title: "1", subTitle1: "Desarrollo", subTitle2: "Joomla" },
-  { title: "5", subTitle1: "Desarrollo", subTitle2: "WIX" },
-  { title: "8", subTitle1: "Desarrollo", subTitle2: "Full Stack" },
-  { title: "51", subTitle1: "Campañas", subTitle2: "Google Ads" },
-  { title: "43", subTitle1: "Campañas", subTitle2: "META" },
-  { title: "41", subTitle1: "Campañas", subTitle2: "Emailing" },
-  { title: "21", subTitle1: "Campañas", subTitle2: "Tiktok" },
-  { title: "2", subTitle1: "Campañas", subTitle2: "Twitter" },
-  { title: "2", subTitle1: "Campañas", subTitle2: "LinkedIn" },
-  { title: "4", subTitle1: "Diseños", subTitle2: "Corporativos" },
+  { title: "245", subTitle1: "Desarrollo", subTitle2: "CMS" },
+  { title: "12", subTitle1: "Desarrollo", subTitle2: "Full Stack" },
+  { title: "51", subTitle1: "Campañas", subTitle2: "SEO" },
+  { title: "43", subTitle1: "Campañas", subTitle2: "SEM" },
+  { title: "7", subTitle1: "Diseños", subTitle2: "Corporativos" },
   { title: "15", subTitle1: "Servidores", subTitle2: "Desplegados" },
+  { title: "∞", subTitle1: "Clientes", subTitle2: "Contentos" },
 ];
 
 const Achievements = () => {
